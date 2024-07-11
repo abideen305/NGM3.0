0e19b1ab2652dbcac6d3ed0ed0b8b2435d6747b2
Abideen Muhammed <abideen.muhammed@afriglobalmedicare.com>
2024-07-11 12:24:44 +0100

    hamburger mobile view

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,36 +1,62 @@
-import React from 'react'
-import Button from './Button'
-import "./search.css"
-import logo from "../assets/images/Logo.png"
-import { Link } from 'react-router-dom'
-import Footer from '../views/Footer'
-import Agenda from '../views/Agenda'
-import Gallery from '../views/Gallery'
-import Home from '../views/Home'
+import React, { useRef, useState } from 'react';
+import Button from './Button';
+import './search.css';
+import logo from '../assets/images/Logo.png';
+import { Link } from 'react-router-dom';
+import Footer from '../views/Footer';
+import Agenda from '../views/Agenda';
+import Gallery from '../views/Gallery';
+import Home from '../views/Home';
 
 const Search = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const hamburgerMenuRef = useRef(null);
+  const mobileNavRef = useRef(null);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-     <div className="flex  justify-center ">
-      <div className="searchBg">
-        <div className="logo ">
-          <img src={logo} alt="logo"/>
-        </div>
-        <div className="cta">
-          <div className="home">
-            <Link to={Home}>Home</Link>
-            <Link to={Agenda}>Event Brochure</Link>
-            <Link to={Footer}>Contact Us</Link>
-          </div>
-         <Link to={Gallery}>Gallery</Link> 
-          <a href="#" className='xs:hidden'>Become a sponsor</a>
-          <a href="https://ngmconference.substack.com">Join NGM community</a>
-        </div>
-          <Button title="Buy Ticket" size={16} pad="10px 20px" />
-      </div>
-     </div>
-    </>
-  )
-}
+      <nav>
+        <div className="flex justify-center">
+          <div className="searchBg">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+            </div>
+            <div className="cta">
+              <div className="home">
+                <Link to={Home}>Home</Link>
+                <Link to={Agenda}>Event Brochure</Link>
+                <Link to={Footer}>Contact Us</Link>
+                <Link to={Gallery}>Gallery</Link>
+                <a href="#" className='xs:hidden'>Become a sponsor</a>
+                <a href="https://ngmconference.substack.com">Join NGM community</a>
+                <a href="https://ngmconference.substack.com"><Button title="Buy Ticket" size={13} pad="5px 10px" /></a>
+              </div>
 
-export default Search
+              <div className="hamburger-menu" id="hamburger-menu" ref={hamburgerMenuRef} onClick={toggleMenu}>
+                <div className={`bar ${isMenuOpen ? 'active' : ''}`}></div>
+                <div className={`bar ${isMenuOpen ? 'active' : ''}`}></div>
+                <div className={`bar ${isMenuOpen ? 'active' : ''}`}></div>
+              </div>
+
+              <div className={`mobile-nav ${isMenuOpen ? 'active' : ''}`} id="mobile-nav" ref={mobileNavRef}>
+                <Link to={Home}>Home</Link>
+                <Link to={Agenda}>Event Brochure</Link>
+                <Link to={Footer}>Contact Us</Link>
+                <Link to={Gallery}>Gallery</Link>
+                <a href="#" className='xs:hidden'>Become a sponsor</a>
+                <a href="https://ngmconference.substack.com">Join NGM community</a>
+                <a href="https://ngmconference.substack.com"><Button title="Buy Ticket" size={16} pad="10px 20px" /></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Search;
